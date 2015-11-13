897809a74f06e6625606d4f18a975b305544570d
Victor Gouron <victor.gouron@hotmail.fr>
2015-11-13 15:58:44 +0100

    Delete old files from HEAD

--- menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<ul>
-<li><a href="user.php">Fil d\'actualite</a></li>
-<li><a href="friends.php">Gerer ses amis</a></li>
-<li><a href="posts.php">Messages</a></li>
-<li><a href="options.php">Options</a></li>
-<li><a href="signout.php?signout=true">Deconnexion</a></li>
-</ul>';
-?>
--- signout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (isset($_GET["signout"])) { //Signout function
-    unset($_SESSION["id"]);
-    header("Location: home.php");
-}
-?>
