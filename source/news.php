c9f881121fda4b316123856edcbb7d1d78d5612a
Victor Gouron <victor.gouron@hotmail.fr>
2015-11-15 19:02:16 +0100

    Recode news.php to show both user and friends posts

--- source/news.php
+++ source/news.php
@@ -1,14 +1,23 @@
 <?php //Affiche les evenements recents en lien avec l'utilisateur
+//On creer un tableau contenant l'id de l'utilisateur et de chacun de ses amis
+$users = array($_SESSION["id"]);
 $query = $db -> prepare("SELECT user2 FROM friends WHERE user1 = ?");
 $query -> execute(array($_SESSION["id"]));
-while ($data = $query -> fetch()) { //On recupere l'id de chaque amis de l'utilisateur
-    $sc_query = $db -> prepare("SELECT * FROM posts WHERE user = ?");
-    $sc_query -> execute(array($data["user2"]));
-    while ($sc_data = $sc_query -> fetch()) { //On affiche chaque messages ecrit par l'ami de l'utilisateur
-	$friend_login = get_user_login($sc_data["user"], $db);
-	echo "Message de " . $friend_login . "<br>";
-	echo $sc_data["title"] . "<br>";
-	echo $sc_data["content"] . "<br><br>";
-    }
+while ($data = $query -> fetch()) {
+    $users[] = $data["user2"];
+}
+$query -> closeCursor();
+
+//On execute une requete pour obtenir chaque messages
+$users_string = implode(",", $users);
+$query = $db -> prepare("SELECT user, title, content FROM posts WHERE user IN ($users_string)"); //Rajouter une limite de nombre et de temps
+$query -> execute($users);
+
+//On affiche tous les posts obtenus
+while ($data = $query -> fetch()) {
+    $login = get_user_login($data["user"], $db);
+    echo "Message de <b>" . $login . "</b> :<br>"; //Afficher la date du post (si possible)
+    echo "<b>" . $data["title"] . "</b><br>";
+    echo $data["content"] . "<br><br>";
 }
 ?>
